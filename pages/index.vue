1f59ac3afb244bbef251254b48aaf7150c993d61
doyoung Hwang <dodo45133@gmail.com>
2019-07-20 01:16:47 +0900

    네이버 인증

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,24 @@
 
 <script>
 export default {
-  layout: 'main'
+  layout: 'main',
+  head() {
+    return {
+      title: `hihi`,
+      meta: [{
+        hid: `iOSUrl`,
+        property: 'al:ios:url'
+      },
+      {
+        hid: `description`,
+        name: 'description'
+      },
+      {
+        name: 'naver-site-verification',
+        content: 'b7e968640f73f091569e1bba8f9dec5d47887e02'
+      }
+      ]
+    }
+  }
 }
 </script>
